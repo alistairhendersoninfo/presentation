60254b8165de5f88e153f9efd1095da1057d5029
Alistair <al@alistairhenderson.info>
2025-08-01 09:27:07 +0100

    updated setup script

--- scripts/setup_nginx.sh
+++ scripts/setup_nginx.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-SITE_NAME="presentation.AlistairHenderson.local"
+
+read -rp "Enter the FQDN for the site (e.g., presentation.AlistairHenderson.local): " SITE_NAME
+
 WEB_ROOT="/var/www/$SITE_NAME"
 NGINX_CONF="/etc/nginx/sites-available/$SITE_NAME"
 NGINX_LINK="/etc/nginx/sites-enabled/$SITE_NAME"
 USER="www-data"
+
 sudo mkdir -p "$WEB_ROOT"
+
 sudo tee "$NGINX_CONF" > /dev/null <<EOF
 server {
     listen 80;
@@ -13,7 +17,7 @@ server {
     index index.php index.html;
     client_max_body_size 100M;
     location / {
-        try_files $uri $uri/ /index.php?$args;
+        try_files \$uri \$uri/ /index.php?\$args;
     }
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
@@ -24,6 +28,7 @@ server {
     }
 }
 EOF
+
 sudo ln -sf "$NGINX_CONF" "$NGINX_LINK"
 sudo chown -R $USER:$USER "$WEB_ROOT"
 sudo nginx -t && sudo systemctl reload nginx
